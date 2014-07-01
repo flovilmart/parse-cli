01f1e238f0dbddff54ac49f5cd94aeead7de0955
Florent Vilmart <vfloz@me.com>
2014-07-01 14:07:29 -0400

    finish integration

---
+++ res/tpl/cloud/main.js
@@ -0,0 +1,5 @@
+// Use Parse.Cloud.define to define as many cloud functions as you want.\n\
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
